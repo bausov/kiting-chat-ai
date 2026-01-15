37315bf29079895cd4b960a93ba4d4cb5ec7450f
bausov <stas909@gmail.com>
2026-01-15 16:13:24 +0300

    init

---
+++ src/test/kotlin/com/github/bausov/kitingchatai/KitingChatAiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.bausov.kitingchatai
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KitingChatAiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
