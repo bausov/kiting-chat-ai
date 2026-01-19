260b5d5d7c7f1cef281f2583ccf8281c206ec05f
bausov <stas909@gmail.com>
2026-01-20 01:19:33 +0300

    mcp-server module init

--- mcp-server/src/test/kotlin/com/github/bausov/kitingchatai/mcp/server/KitingChatAiMcpServerApplicationTests.kt
+++ mcp-server/src/test/kotlin/com/github/bausov/kitingchatai/mcp/server/KitingChatAiMcpServerApplicationTests.kt
@@ -1,4 +1,4 @@
-package com.github.bausov.kitingchatai.host
+package com.github.bausov.kitingchatai.mcp.server
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
