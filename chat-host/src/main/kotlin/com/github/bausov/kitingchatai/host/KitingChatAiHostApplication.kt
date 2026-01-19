ef2c6dd000b3e33befaa475a423ec763c50ac783
bausov <stas909@gmail.com>
2026-01-20 01:26:43 +0300

    rename modules main classes

--- chat-host/src/main/kotlin/com/github/bausov/kitingchatai/host/KitingChatAiHostApplication.kt
+++ chat-host/src/main/kotlin/com/github/bausov/kitingchatai/host/KitingChatAiHostApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class KitingChatAiApplication
+class KitingChatAiHostApplication
 
 fun main(args: Array<String>) {
-    runApplication<KitingChatAiApplication>(*args)
+    runApplication<KitingChatAiHostApplication>(*args)
 }
