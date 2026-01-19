14c6339fe73cf8d863cea07b51d30fd7db80e1ae
bausov <stas909@gmail.com>
2026-01-20 01:12:33 +0300

    mcp-server module init

---
+++ mcp-server/src/main/kotlin/com/github/bausov/kitingchatai/mcp/server/KitingChatAiMcpServerApplication.kt
@@ -0,0 +1,11 @@
+package com.github.bausov.kitingchatai.mcp.server
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KitingChatAiApplication
+
+fun main(args: Array<String>) {
+    runApplication<KitingChatAiApplication>(*args)
+}
---
+++ mcp-server/src/test/kotlin/com/github/bausov/kitingchatai/host/KitingChatAiMcpServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.bausov.kitingchatai.host
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KitingChatAiMcpServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
