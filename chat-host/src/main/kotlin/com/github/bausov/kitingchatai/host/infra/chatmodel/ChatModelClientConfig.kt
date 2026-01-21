fe6e503d6866e79dc7fe941f72a0eea1f3197ca9
bausov <stas909@gmail.com>
2026-01-21 21:34:40 +0300

    McpSampling

--- chat-host/src/main/kotlin/com/github/bausov/kitingchatai/host/infra/chatmodel/ChatModelClientConfig.kt
+++ chat-host/src/main/kotlin/com/github/bausov/kitingchatai/host/infra/chatmodel/ChatModelClientConfig.kt
@@ -1,19 +1,24 @@
 package com.github.bausov.kitingchatai.host.infra.chatmodel
 
+import io.modelcontextprotocol.spec.McpSchema.*
+import org.springaicommunity.mcp.annotation.McpSampling
 import org.springframework.ai.chat.client.ChatClient
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor
 import org.springframework.ai.chat.memory.ChatMemory
+import org.springframework.ai.chat.model.ChatModel
 import org.springframework.ai.ollama.api.OllamaChatOptions
+import org.springframework.ai.tool.ToolCallbackProvider
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class ChatModelClientConfig(
     private val chatMemory: ChatMemory,
+    private val chatModel: ChatModel,
 ) {
 
     @Bean
-    fun chatClient(builder: ChatClient.Builder): ChatClient {
+    fun chatClient(builder: ChatClient.Builder, toolCallbackProvider: ToolCallbackProvider): ChatClient {
         return builder
             .defaultOptions(
                 OllamaChatOptions.builder()
@@ -26,6 +31,27 @@ class ChatModelClientConfig(
             .defaultAdvisors(
                 MessageChatMemoryAdvisor.builder(chatMemory).order(0).build()
             )
+            .defaultToolCallbacks(toolCallbackProvider)
             .build()
+    }
+
+    @McpSampling(clients = ["mcp-server"])
+    fun sampling(createMessageRequest: CreateMessageRequest): CreateMessageResult {
+        val samplingChatClient = ChatClient.builder(chatModel)
+            .defaultOptions(
+                OllamaChatOptions.builder()
+                    .numPredict(createMessageRequest.maxTokens())
+                    .temperature(createMessageRequest.temperature()).build()
+            )
+            .build()
+
+        val samplingAnswer: String = samplingChatClient
+            .prompt()
+            .system(createMessageRequest.systemPrompt())
+            .user(createMessageRequest.messages().first().content().toString())
+            .call()
+            .content()!!
+
+        return CreateMessageResult.builder().content(TextContent(samplingAnswer)).build()
     }
 }
